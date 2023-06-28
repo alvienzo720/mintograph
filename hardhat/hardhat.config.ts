86a16a5c1343a830524df5afb5cba8f3e700c632
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-06-28 13:12:14 +0300

    Modify Hardhat.conf

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,7 +1,16 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import 'dotenv/config'
 
 const config: HardhatUserConfig = {
+  networks: {
+    sepolia: {
+      url: process.env.INFURA_PROJECT_URL,
+      chainId: 4, // Sepolia uses chainId 4
+      accounts: ['process.env.PRIVATE_KEY'], // Your private key
+      gasPrice: 20000000000, // Optional: you can set a custom gas price
+    },
+  },
   solidity: "0.8.18",
 };
 
