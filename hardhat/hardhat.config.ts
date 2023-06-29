7c1fc4798efecf98a32e0e36cd72b75e484205a3
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-06-29 09:54:11 +0300

    Modify Deploy ANd NFT contract

--- hardhat/hardhat.config.ts
+++ hardhat/hardhat.config.ts
@@ -1,14 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import 'dotenv/config'
-import "@nomiclabs/hardhat-ethers";
+
 
 const config: HardhatUserConfig = {
   networks: {
     sepolia: {
       url: process.env.INFURA_PROJECT_URL,
       chainId: 11155111, // Sepolia uses chainId 4
-      accounts: ['process.env.PRIVATEKEY'],// Your private key
+      accounts: [''],// Your private key
       gasPrice: 20000000000, // Optional: you can set a custom gas price
     },
   },
--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -2,8 +2,9 @@ import { ethers } from "hardhat";
 
 async function main() {
   const SimpleNFT = await ethers.getContractFactory('SimpleNFT');
-  const simpleNFT = await SimpleNFT.deploy();
+  const simpleNFT = await SimpleNFT.deploy('0xC2418f0A32ff21Ff4f6D688D3fEe493B5417648a');
   console.log("Contract Deployed to: ", simpleNFT.address);
+
 
 }
 
