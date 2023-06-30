368020e1e63acd7759c587b5103f91dcacd3d053
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-06-30 13:16:15 +0300

    Add connect wallet and get balance bot commands

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -1,9 +1,41 @@
-import {Telegraf} from 'telegraf'
+import {Telegraf, Markup} from 'telegraf'
 import { ConfigParams } from '../config'
+import { connetWallet, walletBalance } from '../controllers/walletController';
+import { sendMessage } from '../utils/telegram';
+
 
 const bot =  new Telegraf(ConfigParams.BOT_TOKEN)
 
-bot.start((ctx)=> ctx.reply("Welcome Connect your wallet and start minting NFTS"))
+bot.start((ctx)=>{ 
+    ctx.reply("Welcome Connect your wallet and start minting NFTS");
+    const custom_keyboard = Markup.inlineKeyboard([
+        [Markup.button.callback('Connect Wallet','connectwallet')],
+        [Markup.button.callback('Wallet Balance', 'walletbalance')],
+        
+    ])
+    ctx.reply('Please have your wallet key ready', { reply_markup: { inline_keyboard: custom_keyboard.reply_markup.inline_keyboard } })
+})
+
+bot.action('connectwallet', async(ctx)=>{
+    try {
+        connetWallet(ConfigParams.PRIVATE_KEY)
+        
+    } catch (error) {
+        let message:any = error
+        sendMessage(message)
+        
+    }
+})
+
+bot.action("walletbalance",async (ctx) => {
+    try {
+        walletBalance(ConfigParams.PRIVATE_KEY)
+    } catch (error:any) {
+       console.log(error)
+        
+    }
+    
+})
 
 export {bot}
 
--- src/controllers/walletController.ts
+++ src/controllers/walletController.ts
@@ -1,18 +1,39 @@
 import { Request, Response } from "express"
-import {ethers} from "ethers"
+import { ethers } from "ethers"
 import { ConfigParams } from "../config";
+import { sendMessage } from "../utils/telegram";
+import { provider } from "../config";
 
-
-export const connetWallet = async (req: Request, res: Response) => {
+export const connetWallet = async (privateKey: any) => {
     try {
-        const privateKey = req.body.privateKey;
-       const provider = new ethers.providers.JsonRpcProvider(ConfigParams.INFURA_URL);
-       const wallet = new ethers.Wallet(privateKey, provider);
-       const address = await wallet.getAddress();
-       res.json({message:`Wallet ${address} connected successfuly`})
+        
+        const wallet = new ethers.Wallet(privateKey, provider);
+        const address = await wallet.getAddress();
+
+        let message = `*\Wallet ${address} connected successfuly\*`
+        // console.log("Address", address)
+        sendMessage(message)
     } catch (error) {
-        console.log("Error", error);
-        res.status(500).json({ message: "Error connecting to wallet" })
+        // console.log("Error", error)
+
+        let message = `*\Error connecting to wallet check the wallet address\*`
+        sendMessage(message)
 
     }
+}
+
+export const walletBalance = async (privateKey:any) => {
+    try {
+        const wallet =  new ethers.Wallet(privateKey,provider)
+        const balance =  await wallet.getBalance();
+        const readablebalance = parseFloat(ethers.utils.formatEther(balance)).toFixed(4);
+        console.log("balance", readablebalance);
+        let message = `*\Balance is : ${readablebalance} ETH\*`
+        sendMessage(message);
+    } catch (error:any) {
+        console.log(error)
+        
+        
+    }
+    
 }
