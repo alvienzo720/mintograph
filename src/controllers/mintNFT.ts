8c59309f0ea3b9a18469f80229e07911e886a502
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-07-02 09:32:42 +0300

    Some changes to bot file

--- src/controllers/mintNFT.ts
+++ src/controllers/mintNFT.ts
@@ -2,9 +2,16 @@ import {ethers} from "ethers";
 import { ABI } from "../config/ABI";
 import { provider } from "../config";
 import { sendMessage } from "../utils/telegram";
+import fs from "fs"
 
+let isUploading = false;
+const uploadToIpfs = async ()=>{
+    
+
+}
 
 export async function mintNFT(privateKey:any,contactAddress:string,tokenURI:string){
+
     const wallet =  new ethers.Wallet(privateKey, provider);
     const contract = new ethers.Contract(contactAddress, ABI, wallet);
     const tx = await contract.mintNFT(tokenURI);
@@ -13,7 +20,7 @@ export async function mintNFT(privateKey:any,contactAddress:string,tokenURI:stri
 
     let message = `NFT minted successfully`
     message += `\n Hash: \`${receipt.transactionHash}\``
-    message += `\n Gas Used: \`${ethers.utils.formatEther(receipt.gasUsed)}\``
+    message += `\n Gas Used: \`${ethers.utils.formatEther(receipt.gasUsed)} ETH\``
     message += `\n To: \` ${receipt.to}\``
     message += `\n From: \` ${receipt.from}\``
     message += `\n View NFT at :https://sepolia.etherscan.io/tx/${receipt.transactionHash}`
