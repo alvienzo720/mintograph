f6a3bf81f69b18ec56b839a24bf814cdfa765da3
MutebiAlvin <mutebialvinalvienzo@gmail.com>
2023-07-03 13:19:29 +0300

    Add Get Transactions By Address function

---
+++ src/controllers/index.ts
@@ -0,0 +1,4 @@
+export * from "./transferNFT";
+export * from "./walletController";
+export * from "./mintNFT";
+export *from "./viewTxns";
---
+++ src/controllers/viewTxns.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { ConfigParams } from "../config";
+import { sendMessage } from "../utils/telegram";
+
+export async function fetchTransactions() {
+    try {
+        const response = await axios.get(`https://api-sepolia.etherscan.io/api?module=account&action=tokentx&address=${ConfigParams.SENDER_ADDRESS}&startblock=0&endblock=999999999&sort=desc&apikey=${ConfigParams.ETH_SCAN_API}`);
+        let transactions: any = response.data.result;
+
+        if (transactions.length > 0) {
+            // Get the last 10 transactions
+            transactions = transactions.slice(0, 10);
+
+            let message = 'Your Transactions:\n\n';
+
+            transactions.forEach((Txn: { tokenSymbol: any; contractAddress: any; from: any; to: any; }) => {
+                message += `Token Symbol: ${Txn.tokenSymbol}\n`;
+                message += `Contract Address: ${Txn.contractAddress}\n`;
+                message += `From: ${Txn.from}\n`;
+                message += `To: ${Txn.to}\n\n`;
+            });
+
+            sendMessage(message)
+        } else {
+            let message = 'No NFT transactions Found for this Address:\n\n';
+            sendMessage(message);
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+}
